c348aba3ba21de0910f7188cf16462bed146079e
mir ali <mir.ali@avalara.com>
2021-12-26 19:22:58 -0800

    JS 12/26

---
+++ JS/01-Fundamentals-part-1/assignments.js
@@ -0,0 +1,17 @@
+const country = "India";
+const continent = "Asia";
+const population = "1400000000";
+console.log(
+  `Country: ${country}, Continent: ${continent}, Population: ${population}`
+);
+
+const isIsland = false;
+let language = "kannada";
+console.log(
+  `Type of Country: ${typeof country}, 
+   Type of Continent: ${typeof continent}, 
+   Type of Population: ${typeof population},
+   Type of isIsland: ${typeof isIsland},
+   Type of language: ${typeof language}`
+);
+language = "Hindi";
